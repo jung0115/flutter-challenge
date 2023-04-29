1b82a91b045dc81c05392cf13e447064a9987364
jung0115 <jungim7490@gmail.com>
2023-04-30 00:41:50 +0900

    Chapter 6: 6.3, 6.4

--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -1,13 +1,18 @@
 // flutter, dart 패키지 찾을 때 -> pub.dev
 // https://pub.dev/packages/http/install
 
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+
     final url = Uri.parse('$baseUrl/$today');
 
     // await: api 작업이 처리될 때까지 기다림
@@ -16,8 +21,16 @@ class ApiService {
 
     // 상태코드가 200 = 성공
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      //print(response.body);
+
+      // JSON으로 변환
+      final List<dynamic> webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+
+      return webtoonInstances;
     }
     throw Error();
   }
